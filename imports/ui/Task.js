83cd6f6a0052692bf08791fc8ec02a44df8e1f2c
Muhsin K <muhsinkeramam@gmail.com>
2020-06-10 08:39:54 +0530

    :sparkles: Complete the applications

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -6,13 +6,23 @@ import { Tasks } from "../api/tasks.js";
 export default class Task extends Component {
   toggleChecked() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this.props.task._id, {
-      $set: { checked: !this.props.task.checked },
-    });
+    Meteor.call(
+      "tasks.setChecked",
+      this.props.task._id,
+      !this.props.task.checked
+    );
   }
 
   deleteThisTask() {
-    Tasks.remove(this.props.task._id);
+    Meteor.call("tasks.remove", this.props.task._id);
+  }
+
+  togglePrivate() {
+    Meteor.call(
+      "tasks.setPrivate",
+      this.props.task._id,
+      !this.props.task.private
+    );
   }
 
   render() {
@@ -32,6 +42,16 @@ export default class Task extends Component {
           checked={!!this.props.task.checked}
           onClick={this.toggleChecked.bind(this)}
         />
+        {this.props.showPrivateButton ? (
+          <button
+            className="toggle-private"
+            onClick={this.togglePrivate.bind(this)}
+          >
+            {this.props.task.private ? "Private" : "Public"}
+          </button>
+        ) : (
+          ""
+        )}
 
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}
